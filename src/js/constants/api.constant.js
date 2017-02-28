273eaa9c31020ad3e0d60e518daf61b44270c7b4
rainede <rainey@techgiver.com>
2017-02-28 17:16:08 +0000

    add /api to URL for API constant src/js/constants/api.constant.js

--- src/js/constants/api.constant.js
+++ src/js/constants/api.constant.js
@@ -1,3 +1,3 @@
 angular
 .module('sosohappy')
-.constant('API',window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : 'http://sohappy.herokuapp.com/');
+.constant('API',window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : 'http://sohappy.herokuapp.com/api');
